99be703df7430b2a7523207d5a06da8a08d163aa
Jonathan Adithya <jo.adithya391@gmail.com>
2023-04-17 19:30:28 -0700

    add todo to app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,12 @@
+import { Todo } from './components/todo/TodoManager';
+
 function App() {
   return (
-    <div className="App">
-      Hello, World!
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-bg gap-8">
+      <h1 className="font-extralight text-[6rem] tracking-[.3rem] text-primary">
+        Todos
+      </h1>
+      <Todo />
     </div>
   );
 }
