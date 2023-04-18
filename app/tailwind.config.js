b84211dba022beeb95abeb19cebe55699541f7f0
Jonathan Adithya <jo.adithya391@gmail.com>
2023-04-17 19:28:16 -0700

    add color theme to tailwind

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#fce1e4",
+        bg: "#fff7f8",
+        gray: "#e1e1e1",
+        dark: {
+          primary: "#cca7ac",
+          gray: "#555",
+        },
+      },
+      keyframes: {
+        ping: {
+          "0%": { backgroundColor: "rgba(252, 225, 228, 0)" },
+          "50%": { backgroundColor: "rgba(252, 225, 228, 1)" },
+          "100%": { backgroundColor: "rgba(252, 225, 228, 0)" },
+        },
+      },
+      animations: {
+        ping: "ping .4s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 }
