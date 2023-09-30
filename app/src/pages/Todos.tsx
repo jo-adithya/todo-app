1c2f7cc2d2eb1550f664f75640136bdb92531b9f
Jonathan Adithya <jo.adithya391@gmail.com>
2023-09-30 16:07:33 -0700

    remove calendar

--- app/src/pages/Todos.tsx
+++ app/src/pages/Todos.tsx
@@ -1,5 +1,4 @@
 import { TodoManager } from "../components/todo/TodoManager";
-import { CalendarManager } from '../components/calendar/CalendarManager';
 
 export const Todos = () => {
   return (
@@ -7,10 +6,7 @@ export const Todos = () => {
       <h1 className="font-extralight text-[6rem] tracking-[.3rem] text-primary">
         Todos
       </h1>
-      <div className="flex gap-16 items-center">
-        <CalendarManager />
-        <TodoManager />
-      </div>
+      <TodoManager />
     </div>
   );
 };
